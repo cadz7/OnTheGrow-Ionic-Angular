85790173a03ff25185ad51d0aad5c04d0fe10f81
Abdella Ali <abdella@rangle.io>
2014-05-18 14:42:39 -0400

    changes to mock data to be slightly more true to schema

--- www/app/leaderboards/leaderboards-ctrl.js
+++ www/app/leaderboards/leaderboards-ctrl.js
@@ -6,12 +6,44 @@ angular.module('sproutApp.controllers')
 
     $scope.leaderboardData = [
       {
-        username: 'Mariah Carey',
+        user_id : 1971,
+        name: 'Mariah',
         department: 'Audio Visual',
         location: 'New York',
         score: 1400,
         rank: 1,
+        isViewer: true,
         userImage: 'img/user/mariah-s.jpg'
+      },
+      {
+        user_id : 1971,
+        name: 'Bruno',
+        department: 'Audio Visual',
+        location: 'New York',
+        score: 500,
+        rank: 4,
+        isViewer: true,
+        userImage: 'img/user/bruno-s.jpg'
+      },
+      {
+        user_id : 1971,
+        name: 'Martin',
+        department: 'Audio Visual',
+        location: 'New York',
+        score: 1100,
+        rank: 2,
+        isViewer: true,
+        userImage: 'img/user/martin-garrix-m.jpg'
+      },
+      {
+        user_id : 1971,
+        name: 'Slash',
+        department: 'Audio Visual',
+        location: 'New York',
+        score: 850,
+        rank: 3,
+        isViewer: true,
+        userImage: 'img/user/slash-s.jpg'
       }
     ];
   }]);
