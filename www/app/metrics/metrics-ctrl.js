aad20653d748006a5b9b2ca292d1272defc2dc6e
John Sintal <john.sintal@gmail.com>
2014-05-17 20:55:35 -0400

    dynamic list view of activity logs

--- www/app/metrics/metrics-ctrl.js
+++ www/app/metrics/metrics-ctrl.js
@@ -1,7 +1,37 @@
 'use strict';
 
 angular.module('sproutApp.controllers')
-.controller('MetricsCtrl', ['$scope', 'headerRemote', function($scope, headerRemote) {
-		$scope.header = headerRemote;
-    $scope.radioModel = 'Week';
-}]);
+  .controller('MetricsCtrl', ['$scope', 'headerRemote', function ($scope, headerRemote) {
+    $scope.header = headerRemote;
+    $scope.logPeriodFilter = 'Week';
+
+    // TODO please note that this may not be the final model and will be wrapped in async service
+    $scope.user = {
+      firstName: 'John',
+      lastName: 'Sintal',
+      department: 'Department',
+      location: 'Location',
+      totalScore: '1500'
+    };
+
+    // TODO please note that this may not be the final model and will be wrapped in async service
+    $scope.groupedActivities = [
+      {
+        timePeriod: 'Week 1',
+        activities: [
+          {
+            duration: 12,
+            points: 166,
+            title: 'Cycling'
+          },
+          {
+            duration: 2,
+            points: 33,
+            title: 'Baseball'
+          }
+        ]
+      }
+    ];
+
+
+  }]);
