24ded036873ea2240d4d6fca80b1d8eb5f215504
Abdella Ali <abdella@rangle.io>
2014-05-18 13:13:15 -0400

    proper commit

--- www/app/leaderboards/leaderboards-ctrl.js
+++ www/app/leaderboards/leaderboards-ctrl.js
@@ -1,6 +1,16 @@
 'use strict';
 
 angular.module('sproutApp.controllers')
-.controller('LeaderboardsCtrl', ['$scope', 'headerRemote', function($scope, headerRemote) {
-		$scope.header = headerRemote;
-}]);
+  .controller('LeaderboardsCtrl', ['$scope', 'headerRemote', function($scope, headerRemote) {
+    $scope.header = headerRemote;
+
+    $scope.leaderboardData = [
+      {
+        username: 'Bob',
+        department: 'Sanitation',
+        location: 'New Orleans',
+        score: 1400,
+        rank: 1
+      }
+    ];
+  }]);
