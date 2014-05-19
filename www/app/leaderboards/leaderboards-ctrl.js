6c3bcaf40fe6a4915b381dd7f2c0f0e259a4df38
Abdella Ali <abdella@rangle.io>
2014-05-19 14:06:53 -0400

    wire up leaderboard service to controller

--- www/app/leaderboards/leaderboards-ctrl.js
+++ www/app/leaderboards/leaderboards-ctrl.js
@@ -1,78 +1,56 @@
 'use strict';
 
 angular.module('sproutApp.controllers')
-  .controller('LeaderboardsCtrl', ['$scope', 'headerRemote', '$ionicActionSheet',
-    function($scope, headerRemote, $ionicActionSheet) {
-    $scope.header = headerRemote;
-    $scope.leaderboardFilter = 'Overall';
+  .controller('LeaderboardsCtrl', ['$scope', 'headerRemote', '$ionicActionSheet', 'leaderboards',
+    function ($scope, headerRemote, $ionicActionSheet, leaderboards) {
+      $scope.header = headerRemote;
 
-    $scope.showLeaderboardFilter = function(){
+      $scope.leaderboardFilter = 'Overall';
 
-    $ionicActionSheet.show({
-      titleText: 'Filter By Type:',
-      // buttons: filters,
-      buttons: [{
-        text: 'Overall'
-      }, {
-        text: 'Challenge 1'
-      }, {
-        text: 'Challenge 2'
-      }, {
-        text: 'Challenge 3'
-      }, {
-        text: 'Challenge 4'
-      }],
-      cancelText: 'Back',
-      cancel: function() {
-        return true;
-      },
-      buttonClicked: function(index) {
-        $scope.leaderboardFilter = this.buttons[index].text;
-        return true;
-      }
-    });
-  };
+      var defaultLeaderboardParams = {
+        periodId: 101,
+        userFilterId: 201,
+        activityFilterId: 301
+      };
 
-    $scope.leaderboardData = [
-      {
-        user_id : 1971,
-        name: 'Mariah',
-        department: 'Audio Visual',
-        location: 'New York',
-        score: 1400,
-        rank: 1,
-        isViewer: true,
-        userImage: 'img/user/mariah-s.jpg'
-      },
-      {
-        user_id : 1971,
-        name: 'Bruno',
-        department: 'Audio Visual',
-        location: 'New York',
-        score: 500,
-        rank: 4,
-        isViewer: true,
-        userImage: 'img/user/bruno-s.jpg'
-      },
-      {
-        user_id : 1971,
-        name: 'Martin',
-        department: 'Audio Visual',
-        location: 'New York',
-        score: 1100,
-        rank: 2,
-        isViewer: true,
-        userImage: 'img/user/martin-garrix-m.jpg'
-      },
-      {
-        user_id : 1971,
-        name: 'Slash',
-        department: 'Audio Visual',
-        location: 'New York',
-        score: 850,
-        rank: 3,
-        isViewer: true,
-        userImage: 'img/user/slash-s.jpg'
-      }
-    ];
-  }]);
+      leaderboards.getBoard(defaultLeaderboardParams).then(function(response){
+        $scope.leaderboardData = response;
+        console.log(response);
+      });
+
+
+
+      $scope.showLeaderboardFilter = function () {
+
+        $ionicActionSheet.show({
+          titleText: 'Filter By Type:',
+          // buttons: filters,
+          buttons: [
+            {
+              text: 'Overall'
+            },
+            {
+              text: 'Challenge 1'
+            },
+            {
+              text: 'Challenge 2'
+            },
+            {
+              text: 'Challenge 3'
+            },
+            {
+              text: 'Challenge 4'
+            }
+          ],
+          cancelText: 'Back',
+          cancel: function () {
+            return true;
+          },
+          buttonClicked: function (index) {
+            $scope.leaderboardFilter = this.buttons[index].text;
+            return true;
+          }
+        });
+      };
+
+    }]);
