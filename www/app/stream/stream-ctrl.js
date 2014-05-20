ae2b056a8523ad9ef077e4e5c2cde72eecd0233f
Nitin Vericherla <nitin.vericherla@gmail.com>
2014-05-19 23:32:16 -0400

    splitting out group and events post based on main stream item type

--- www/app/stream/stream-ctrl.js
+++ www/app/stream/stream-ctrl.js
@@ -31,15 +31,29 @@ angular.module('sproutApp.controllers')
         });
       }, 250);
 
+      var editPostScope = $scope.$new(),
+          fullPostScope = $scope.$new();
+
+      fullPostScope.likePost = function(post) {
+        post[post.viewer.isLikedByViewer ? "unlikePost" : "likePost"]().then(
+          function() {
+            $log.debug("success toggling like");
+          },
+          function(err) {
+            $log.error(err);
+          }
+        )
+      };
+
       $ionicModal.fromTemplateUrl('app/stream/post/edit-post-modal.tpl.html', {
-        scope: $scope,
+        scope: editPostScope,
         animation: 'slide-in-up'
       }).then(function(modal) {
         $scope.modal = modal;
       });
 
       $ionicModal.fromTemplateUrl('app/stream/post/full-post-modal.tpl.html', {
-        scope: $scope,
+        scope: fullPostScope,
         animation: 'slide-in-up'
       }).then(function(modal) {
         $scope.dialog = modal;
@@ -60,7 +74,7 @@ angular.module('sproutApp.controllers')
           buttonClicked: function(index) {
             switch (index) {
               case 0: // hide current post
-                stream.deletePost(item).then(function() {
+                streamItems.deletePost(item).then(function() {
                     console.log('Your post has been deleted.');
                   }, function(response) {
                     if (response.status === 403) {
