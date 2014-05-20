bd8c8eab1a779fcd8cfa8b5dce47b56b693f2fd0
Rafael Khan <raf.khan.f@gmail.com>
2014-05-19 22:57:45 -0400

    Committing I did something to the css.. god help me

--- www/app/stream/add-activity-bar/activity-list-dir.js
+++ www/app/stream/add-activity-bar/activity-list-dir.js
@@ -1,13 +1,27 @@
 angular.module('sproutApp.directives')
 .directive('activityList', [function() {
   return {
+    scope: true,
     restrict: 'E',
     templateUrl: 'app/stream/add-activity-bar/activity-list.tpl.html',
     link: function(scope, elem, attrs) {
-      scope.x = 'y';
       scope.title = "lol";
 
-      scope.items = [1,2,3,4,5,6,7,8,9,10];
+      var state = 'categorySelect';
+
+      scope.activityData = scope[attrs.listData];
+      scope.nameKey      = attrs.nameKey;
+
+      scope.onItemSelect = function(item) {
+        if(state === 'categorySelect') {
+          scope.title = item['activityCategoryDisplayName'];
+          scope.activityData = item['activities'];
+          scope.nameKey = 'activityName';
+          state = 'activitySelect';
+        } else if(state === 'activitySelect') {
+          console.log(item);
+        }
+      };
     }
   };
 }]);
