98d2f17d7d5daeabdb85af6267aa1e1bae6f51ab
Rafael Khan <raf.khan.f@gmail.com>
2014-05-28 16:08:25 -0400

    Post box scroll behaviour fix, content still stuck

--- www/app/stream/add-activity-bar/activity-bar-ctrl.js
+++ www/app/stream/add-activity-bar/activity-bar-ctrl.js
@@ -209,47 +209,4 @@ angular.module('sproutApp.controllers')
     $scope.amEditing = item;
     $scope.states[1].selectFunction(item);
   }
-}])
-
-/*
- * Shrinking/fading header directive
- */
-.directive('headerShrink', ['$document', function($document) {
-  var fadeAmt;
-
-  var streamContent = $document[0].getElementById('sproutPostsScroller');
-
-  var shrink = function(header, content, amt, max) {
-    amt = Math.min(120, amt);
-    fadeAmt = 1 - amt / 120;
-    ionic.requestAnimationFrame(function() {
-      header.style[ionic.CSS.TRANSFORM] = 'translate3d(0, -' + amt + 'px, 0)';
-      streamContent.style[ionic.CSS.TRANSFORM] = 'translate3d(0, -' + amt + 'px, 0)';
-
-      for(var i = 0, j = header.children.length; i < j; i++) {
-        header.children[i].style.opacity = fadeAmt;
-      }
-    });
-  };
-
-  return {
-    restrict: 'A',
-    link: function($scope, $element, $attr) {
-      var starty = $scope.$eval($attr.headerShrink) || 0;
-      var shrinkAmt;
-      
-      var header = $document[0].body.querySelector('.add-activity-post');
-      var headerHeight = header.offsetHeight;
-      
-      $element.bind('scroll', function(e) {
-        if(e.detail.scrollTop > starty) {
-          // Start shrinking
-          shrinkAmt = headerHeight - Math.max(0, (starty + headerHeight) - e.detail.scrollTop);
-          shrink(header, $element[0], shrinkAmt, headerHeight);
-        } else {
-          shrink(header, $element[0], 0, headerHeight);
-        }
-      });
-    }
-  };
 }]);
