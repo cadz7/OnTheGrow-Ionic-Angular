2fbc254ceddec1ea4f8abf595ce990ba196dfbee
Rick Shukla <shuklapossible@gmail.com>
2014-08-03 22:58:32 -0400

    Implemented Auth service

--- www/controllers/login-ctrl.js
+++ www/controllers/login-ctrl.js
@@ -2,12 +2,13 @@
 
 angular.module('OnTheGrow.controllers')
 // ----------------------------------------
-// Controller for the browse view
+// Controller for the Login view
 // ----------------------------------------
-.controller('LoginCtrl', ['$scope', '$state', function($scope, $state) {
-  $scope.apply = function() {
-    //$state.go('app.lists');
-    var url = "https://www.sandbox.paypal.com/cgi-bin/webscr?business=tamerlan4572-facilitator@gmail.com&cmd=_xclick&currency_code=USD&amount=20&item_name=Fruits";
-    var ref = window.open(url, '_blank', 'location=no');
-  };
+.controller('LoginCtrl', ['$scope', 'Auth', function($scope, Auth) {
+    $scope.login = function() {
+      Auth.login({
+        email: $scope.email,
+        password: $scope.password
+      });
+    };
 }])
--- www/controllers/signup-ctrl.js
+++ www/controllers/signup-ctrl.js
@@ -2,12 +2,13 @@
 
 angular.module('OnTheGrow.controllers')
 // ----------------------------------------
-// Controller for the browse view
+// Controller for the Signup view
 // ----------------------------------------
-.controller('SignupCtrl', ['$scope', '$state', function($scope, $state) {
-  $scope.apply = function() {
-    //$state.go('app.lists');
-    var url = "https://www.sandbox.paypal.com/cgi-bin/webscr?business=tamerlan4572-facilitator@gmail.com&cmd=_xclick&currency_code=USD&amount=20&item_name=Fruits";
-    var ref = window.open(url, '_blank', 'location=no');
-  };
+.controller('SignupCtrl', ['$scope', 'Auth', function($scope, Auth)
+    $scope.signup = function() {
+      Auth.signup({
+        email: $scope.email,
+        password: $scope.password
+      });
+    };
 }])
