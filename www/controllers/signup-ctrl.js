6b4f3c05202b604016e2fdab57a53113c7e37436
Rick Shukla <shuklapossible@gmail.com>
2014-08-07 23:58:13 -0400

    Implemented of posting of produce from the client to server

--- www/controllers/signup-ctrl.js
+++ www/controllers/signup-ctrl.js
@@ -4,9 +4,16 @@ angular.module('OnTheGrow.controllers')
 // ----------------------------------------
 .controller('SignupCtrl', ['$scope', 'Auth', function($scope, Auth) {
     $scope.signup = function(user) {
+    console.log(user);
       Auth.signup({
         email: user.email,
-        password: user.password
+        password: user.password,
+	    title: user.title,
+	    produceName: user.produceName,
+	    quantity: user.quantity,
+	    price: user.price,
+	    desc: user.desc,
+	    date: user.date
       });
     };
 }])
