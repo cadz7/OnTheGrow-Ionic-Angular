a1c8d7df68752f072fb993a476c4a96002bf12e1
Rick Shukla <shuklapossible@gmail.com>
2014-08-07 23:56:38 -0400

    Changed some specs of User schema

--- server.js
+++ server.js
@@ -23,10 +23,8 @@ var userSchema = new mongoose.Schema({
   password: String,
   listings: [{
       title: String,
-      _id: Number,
-      name: String,
+      produceName: String,
       quantity: Number,
-      image: String,  
       price: Number,
       desc: String, 
       date: Date
@@ -118,13 +116,22 @@ app.post('/api/signup', function(req, res, next) {
   console.log(req.body.password);
   var user = new User({
     email: req.body.email,
-    password: req.body.password
+    password: req.body.password,
+    listings: [{
+      title: req.body.title,
+      produceName: req.body.produceName,
+      quantity: req.body.quantity,
+      price: req.body.price,
+      desc: req.body.desc,
+      date: req.body.date
+    }]
   });
   user.save(function(err) {
     if (err) return next(err);
     res.send(200);
   });
 });
+
 
 app.get('/api/lists', function(req, res) {
     var query = User.find();
