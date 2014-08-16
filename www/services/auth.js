45bfcd6147b228dc961e5ed85916380c543777ef
Rick Shukla <shuklapossible@gmail.com>
2014-08-15 23:56:14 -0400

    Made changed to login screen pointing to live

--- www/services/auth.js
+++ www/services/auth.js
@@ -2,28 +2,35 @@ angular.module('OnTheGrow.services')
 
 /* Auth Service from tracker */
 
-  .factory('Auth', ['$http', '$location', '$rootScope', '$ionicPopup', '$window', '$log', 'SERVER_URL',
-    function($http, $location, $rootScope, $ionicPopup, $window, $log, SERVER_URL) {
+  .factory('Auth', ['$http', '$location', '$rootScope', '$ionicPopup', '$window', '$log', 'SERVER_URL', '$ionicLoading',
+    function($http, $location, $rootScope, $ionicPopup, $window, $log, SERVER_URL, $ionicLoading) {
       $rootScope.currentUser = $window.localStorage['user'];
       $window.localStorage['user'] = '';
 
       return {
         login: function(user) {
+            $ionicLoading.show({
+              template: 'Attempting login... <br><br> <i class="icon ion-loading-c"></i>'
+            });
           return $http.post(SERVER_URL + '/api/login', user)
             .success(function(data) {
+              $ionicLoading.hide();
               $log.log(data);
               $rootScope.currentUser = user;
-              $location.path('/app/landing');
+              $location.path('/app/lists');
 
-              var alertPopup = $ionicPopup.alert({
-                 title: 'You just logged in!',
-                 template: 'Thank you for logging in!'
-               });
-               alertPopup.then(function(res) {
-                 console.log('User logged in');
-               });
+              $ionicLoading.show({
+                  template: '<p class="padding text-center"> Welcome! <br> View listings of local produce <br> or <br> Find them on the map! </p>',
+                  delay: 100,
+                  duration: 3000
+                });
             })
             .error(function(data, status, headers, config) {
+                  $ionicLoading.show({
+                    template: 'Incorrect username or password!',
+                    delay: 1000,
+                    duration: 1500
+                  });
               // called asynchronously if an error occurs
               // or server returns response with an error status.
             });
@@ -53,17 +60,20 @@ angular.module('OnTheGrow.services')
             });
         },
         logout: function() {
+           $ionicLoading.show({
+              template: 'Logging out... <br><br> <i class="icon ion-loading-c"></i>'
+            });
           return $http.get(SERVER_URL + '/api/logout').success(function() {
+            $ionicLoading.hide();
             $rootScope.currentUser = null;
             delete $window.localStorage['user'];
-              var alertPopup = $ionicPopup.alert({
-                 title: 'You just logged out',
-                 template: 'Thank you for using OnTheGrow!'
-               });
-               alertPopup.then(function(res) {
-                 console.log('User Logged out');
-               });
-          });
+          })
+          .error(function(data, status, headers, config) {
+              $ionicLoading.show({
+                template: 'Couldn\'t log out due to server issues.',
+                duration: 1500
+              });
+            });
         }
       };
 }]);
